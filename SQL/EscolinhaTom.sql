43f7d8230a756866e97374bc87c46f6b80647f58
EvertonSPTech <everton.xavier@sptech.school>
2023-05-09 22:59:22 -0300

    Atualizando o Site e implementando a API web-data-viz

---
+++ SQL/EscolinhaTom.sql
@@ -0,0 +1,53 @@
+CREATE DATABASE EscolinhaTom;
+
+USE EscolinhaTom;
+
+CREATE TABLE Escolinha(
+	idEscolinha INT PRIMARY KEY AUTO_INCREMENT,
+    nomeTime VARCHAR(45),
+    email VARCHAR(45),
+    telefoneFixo CHAR(10),
+    telefoneCelular CHAR(11),
+    CEP CHAR (8),
+    Estado VARCHAR (45),
+    Cidade VARCHAR (45),
+    Bairro VARCHAR (45),
+    Rua VARCHAR (45),
+    Numero VARCHAR (45)
+);
+
+CREATE TABLE Professor(
+	idProfessor INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(45),
+    email VARCHAR(45),
+    telefoneCelular CHAR(11),
+    Senha CHAR(11),
+    fkEscolinha INT, CONSTRAINT fkProfessorEscolinha FOREIGN KEY (fkEscolinha) REFERENCES Escolinha (idEscolinha)
+);
+
+CREATE TABLE Aluno(
+	idAluno INT PRIMARY KEY AUTO_INCREMENT,
+    nomeAluno VARCHAR(45),
+    emailAluno VARCHAR(45),
+    TelefoneCelular CHAR(11),
+    DataNasc DATE,
+    Posicao VARCHAR (45),
+    Senha CHAR (8),
+    fkEscolinha INT, CONSTRAINT fkAlunoEscolinha FOREIGN KEY (fkEscolinha) REFERENCES Escolinha (idEscolinha)
+);
+
+CREATE TABLE Treino(
+	idTreino INT,
+    Tipo VARCHAR(45),
+    TamanhoTreino CHAR(1), CONSTRAINT ckTramanhoTreino CHECK (TamanhoTreino IN ('P', 'M', 'G'))
+);
+
+CREATE TABLE Sessao(
+	idSessao INT,
+    DataSessao DATETIME,
+    Duracao TIME,
+    NotaAluno INT,
+    fkTreino INT, CONSTRAINT fkTreinoSessao FOREIGN KEY (fkTreino) REFERENCES Treino (idTreino),
+    fkAluno INT, CONSTRAINT fkSessaoAluno FOREIGN KEY (fkAluno) REFERENCES Aluno (idAluno),
+    CONSTRAINT pkSessao PRIMARY KEY (idSessao, fkAluno)
+);
---
+++ Site/src/routes/usuarios.js
@@ -0,0 +1,32 @@
+var express = require("express");
+var router = express.Router();
+
+var usuarioController = require("../controllers/usuarioController");
+
+router.get("/", function (req, res) {
+    usuarioController.testar(req, res);
+});
+
+router.get("/listar", function (req, res) {
+    usuarioController.listar(req, res);
+});
+
+//Recebendo os dados do html e direcionando para a função cadastrar de usuarioController.js
+router.post("/cadastrarAluno", function (req, res) {
+    usuarioController.cadastrarAluno(req, res);
+})
+
+router.post("/cadastrarEscolinha", function (req, res) {
+    usuarioController.cadastrarEscolinha(req, res);
+})
+
+router.post("/cadastrarProfessor", function (req, res) {
+    usuarioController.cadastrarProfessor(req, res);
+})
+
+router.post("/autenticarAluno", function (req, res) {
+    usuarioController.entrarAluno(req, res);
+});
+
+
+module.exports = router;
